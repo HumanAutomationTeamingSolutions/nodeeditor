8731d260d3278d399ac5e05a2ea4590e9df5969c
Karanvir <karanvir.panesar@hats.solutions>
2017-06-29 11:03:01 -0700

    Removed the qsrand

--- src/ConnectionStyle.cpp
+++ src/ConnectionStyle.cpp
@@ -158,16 +158,17 @@ QColor
 ConnectionStyle::
 normalColor(QString typeId) const
 {
-    std::size_t hash = qHash(typeId);
+  std::size_t hash = qHash(typeId, 50);
+  std::size_t const hue_range = 0xFF;
 
-    std::size_t const hue_range = 0xFF;
+  //qsrand(hash);
+  //std::size_t hue = qrand() % hue_range;
+  std::size_t hue = hash % hue_range;
 
-    qsrand(hash);
-    std::size_t hue = qrand() % hue_range;
+  //std::size_t sat = 120 + hash % 129;
+  std::size_t sat = 90 + hash % 129;
 
-    std::size_t sat = 120 + hash % 129;
-
-    return QColor::fromHsl(hue,
+  return QColor::fromHsl(hue,
                          sat,
                          160);
 }
